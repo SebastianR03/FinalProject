a0f3047a2eef9095a45b26dc87d8ad2d12916be8
Sebastian Rodriguez <sebasrodriguez03@outlook.com>
2023-12-02 12:54:04 -0600

    player movement

--- src/projectFinal.py
+++ src/projectFinal.py
@@ -9,7 +9,7 @@ pygame.display.set_caption("Don't Die")
 back_ground = pygame.image.load("spaceBackground.jpg")
 player_width = 40
 player_height = 60
-
+player_velocity = 5
 
 def draw(player):
     win.blit(back_ground, (0, 0))
@@ -21,14 +21,24 @@ def draw(player):
 def main():
     run = True
 
-    player = pygame.Rect(200, height - player_height, 
+    player = pygame.Rect((width/2), height - player_height, 
                          player_width, player_height)
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - player_velocity >= 0:
+            player.x -= player_velocity
+        if keys[pygame.K_RIGHT] and player.x + player_velocity + player.width <= width:
+            player.x += player_velocity
+
         draw(player)
 
     pygame.quit()

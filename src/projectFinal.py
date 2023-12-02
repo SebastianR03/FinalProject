14f838105b57b94056190f678ab9c5afcd93a5b0
Sebastian Rodriguez <sebasrodriguez03@outlook.com>
2023-12-02 13:01:55 -0600

    added timer

--- src/projectFinal.py
+++ src/projectFinal.py
@@ -3,6 +3,8 @@ import time
 import random
 
 
+pygame.font.init()
+font = pygame.font.SysFont("ariel", 30)
 width, height = 960, 960
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Don't Die")
@@ -11,9 +13,14 @@ player_width = 40
 player_height = 60
 player_velocity = 5
 
-def draw(player):
+def draw(player, elapsed_time):
     win.blit(back_ground, (0, 0))
+
+    time_text = font.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    win.blit(time_text, (10, 10))
+
     pygame.draw.rect(win, "white", player)
+
     pygame.display.update()
 
 
@@ -25,8 +32,12 @@ def main():
                          player_width, player_height)
     clock = pygame.time.Clock()
 
+    start_time = time.time()
+    elapsed_time = 0
+
     while run:
         clock.tick(60)
+        elapsed_time = time.time() - start_time
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -39,7 +50,7 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + player_velocity + player.width <= width:
             player.x += player_velocity
 
-        draw(player)
+        draw(player, elapsed_time)
 
     pygame.quit()
 

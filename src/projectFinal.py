6e085591edf1782323c5e5a8eb88eef47a80a632
Sebastian Rodriguez <sebasrodriguez03@outlook.com>
2023-12-02 13:14:43 -0600

    adding shooting stars

--- src/projectFinal.py
+++ src/projectFinal.py
@@ -9,11 +9,17 @@ width, height = 960, 960
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Don't Die")
 back_ground = pygame.image.load("spaceBackground.jpg")
+
 player_width = 40
 player_height = 60
 player_velocity = 5
 
-def draw(player, elapsed_time):
+star_width = 10
+star_height = 20
+hit = False
+star_velocity = 3
+
+def draw(player, elapsed_time, stars):
     win.blit(back_ground, (0, 0))
 
     time_text = font.render(f"Time: {round(elapsed_time)}s", 1, "white")
@@ -21,7 +27,11 @@ def draw(player, elapsed_time):
 
     pygame.draw.rect(win, "white", player)
 
+    for star in stars:
+        pygame.draw.rect(win, "white", star)
+
     pygame.display.update()
+
 
 
 ####main game loop
@@ -35,9 +45,22 @@ def main():
     start_time = time.time()
     elapsed_time = 0
 
+    star_add_increment = 2000
+    star_count = 0
+
+    stars= []
+
     while run:
-        clock.tick(60)
+        star_count += clock.tick(60)
         elapsed_time = time.time() - start_time
+
+        if star_count > star_add_increment:
+            for _ in range(3):
+                star_x = random.randint(0, width - star_width)
+                star = pygame.Rect(star_x, -star_height, 
+                                   star_width, star_height)
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -50,7 +73,17 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + player_velocity + player.width <= width:
             player.x += player_velocity
 
-        draw(player, elapsed_time)
+        for star in stars[:]:
+            star.y += star_velocity
+            if star.y > height:
+                stars.remove(star)
+            elif star.y + star.height>= player.y and star.colliderect(player):
+                stars.remove(star)
+                hit = True
+                break
+
+
+        draw(player, elapsed_time, stars)
 
     pygame.quit()
 
